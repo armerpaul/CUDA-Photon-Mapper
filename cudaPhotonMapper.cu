769221c396f5251b4d3570cf518a770feaf5ca9e
Paul Armer <armerpaul@gmail.com>
2013-03-07 17:23:07 -0800

    added ray tracer function

--- cudaPhotonMapper.cu
+++ cudaPhotonMapper.cu
@@ -250,6 +250,9 @@ extern "C" void misc(unsigned char key)
 	}
 }
 
+/*
+ * Function Wrapper for the kernel that shoots out photons
+ */
 extern "C" void photonLaunch()
 {
 	Point move;
@@ -426,8 +429,148 @@ Plane* CreatePlanes() {
 	return planes;
 }
 
+
 /*
  * CUDA global function which performs ray tracing. Responsible for initializing and writing to output vector
+ */
+__global__ void renderScene(Camera * cam,Plane * f,PointLight * l, Sphere * s, uchar4 * pos)
+{
+	float tanVal = tan(FOV/2);
+
+	//CALCULATE ABSOLUTE ROW,COL
+	int row = blockIdx.y * blockDim.y + threadIdx.y;
+	int col = blockIdx.x * blockDim.x + threadIdx.x;
+	color_t returnColor;
+	Ray r;
+
+	//BOUNDARY CHECK
+	if(row >= WINDOW_HEIGHT || col >= WINDOW_WIDTH)
+		return;
+
+	float rvaly = tanVal - (2 * tanVal / WINDOW_HEIGHT) * row;
+	float rvalx = -1 * WINDOW_WIDTH / WINDOW_HEIGHT * tanVal + (2 * tanVal / WINDOW_HEIGHT) * col;
+	//INIT RAY VALUES
+	r.origin = cam->eye;
+	r.direction = cam->lookAt;
+	r.direction += (rvalx * cam->lookRight);
+	r.direction += (rvaly * cam->lookUp);
+	r.direction = glm::normalize(r.direction);
+	//r.direction.y += tanVal - (2 * tanVal / WINDOW_HEIGHT) * row;
+	//r.direction.x += -1 * WINDOW_WIDTH / WINDOW_HEIGHT * tanVal + (2 * tanVal / WINDOW_HEIGHT) * col;
+
+	//RAY TRACE
+	returnColor = RayTrace(r, s, f, l);
+
+	//CALC OUTPUT INDEX
+	int index = row *WINDOW_WIDTH + col;
+
+	//PLACE DATA IN INDEX
+	pos[index].x = 0xFF * returnColor.r;
+	pos[index].y = 0xFF * returnColor.g;
+	pos[index].z = 0xFF * returnColor.b;
+	pos[index].w = 0xFF * returnColor.f;
+
+}
+
+/*
+ * Performs Ray tracing over all spheres for any ray r
+ */
+__device__ color_t RayTrace(Ray r, Sphere* s, Plane* f, PointLight* l) {
+	color_t color = CreateColor(0, 0, 0); 
+	float t, smallest;
+	int i = 0, closestSphere = -1, closestPlane = -1,  inShadow = false;
+	//r.direction += r.origin; //Set back to normal
+	Point normalVector;
+	//FIND CLOSEST SPHERE ALONG RAY R
+	while (i < NUM_SPHERES) {
+		t = SphereRayIntersection(s + i, r);
+
+		if (t > 0 && (closestSphere < 0 || t < smallest)) {
+			smallest = t;
+			closestSphere = i;
+		}
+		i++;
+	}
+	//r.direction -= r.origin;
+	i=0;
+	while (i < NUM_PLANES) {
+		t = PlaneRayIntersection(f + i, r);
+		if (t > 0 && ( (closestSphere < 0 && closestPlane < 0) || t < smallest)) {//POSSIBLE LOGIC FIX CLOSESTSPHERE >1
+			smallest = t;
+			closestSphere = -1;
+			closestPlane = i;
+		}
+		i++;
+	}
+
+	//SETUP FOR SHADOW CALCULATIONS
+	i = 0;
+	Ray shadowRay;
+
+	//r.direction += r.origin;//Smallest needs to be calculated differently
+	shadowRay.origin = CreatePoint(r.direction.x * smallest, r.direction.y * smallest, r.direction.z * smallest);
+	shadowRay.origin += r.origin;
+	shadowRay.direction = l->position - shadowRay.origin;
+
+	//DETERMINE IF SPHERE IS BLOCKING RAY FROM LIGHT TO SPHERE
+	if(closestSphere > -1 || closestPlane > -1)
+	{
+		while (i <NUM_SPHERES-1 && !inShadow){ 
+			t = SphereRayIntersection(s + i, shadowRay);
+			if(i != closestSphere && t < 1 && t > 0){
+				//	printf("%f\n",t);
+				inShadow = true;
+			}
+			i++;
+		}
+		i = 0;
+		while(i < NUM_PLANES && !inShadow){
+			t = PlaneRayIntersection(f + i, shadowRay);
+			if(i != closestPlane && t < 1 && t > 0){
+				inShadow = true;
+			}
+			i++;
+		}
+	}
+
+
+	//inShadow = false; 
+	if(closestPlane > -1 && !inShadow)
+	{
+		//plane closer than sphere
+		//normalVector = glm::normalize(f[closestPlane].normal-f[closestPlane].center);
+		return Shading(r, shadowRay.origin, f[closestPlane].normal, l, f[closestPlane].diffuse,
+				f[closestPlane].ambient,f[closestPlane].specular);
+	}
+	if(closestPlane > -1)
+	{
+		color.r = l->ambient.r * f[closestPlane].ambient.r;
+		color.g = l->ambient.g * f[closestPlane].ambient.g;
+		color.b = l->ambient.b * f[closestPlane].ambient.b;
+		//return CreateColor(1,1,1);
+		return color;
+	}
+
+	//IF SHADOWED, ONLY SHOW AMBIENT LIGHTING
+	if(closestSphere > -1 && !inShadow)
+	{
+
+		normalVector = glm::normalize(shadowRay.origin-(s[closestSphere].center));
+		return Shading(r, shadowRay.origin, normalVector, l, s[closestSphere].diffuse,
+				s[closestSphere].ambient,s[closestSphere].specular);
+	}
+	if(closestSphere > -1)
+	{
+		color.r = l->ambient.r * s[closestSphere].ambient.r;
+		color.g = l->ambient.g * s[closestSphere].ambient.g;
+		color.b = l->ambient.b * s[closestSphere].ambient.b;
+	}
+	return color;
+}
+
+
+/*
+ * CUDA global function which performs photon mappning. Responsible for initializing and writing to output vector
  */
 __global__ void CUDAPhotonTrace(Plane * f, RectLight * l, Sphere * s, Photon * pos)
 {
